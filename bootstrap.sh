326c44fd7ae513949f9c715d1086c6538e8eddba
BorkStick <4ciiDicBa55@gmail.com>
2019-09-05 19:26:30 -0500

    tweaked the bootstrap for the new folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script creates symlinks from ~/ to dotfiles dotdir
 
-dotdir=~/dotfiles
-dotbak=~/dotfiles_old
+dotdir=~/.dotfiles
+dotbak=~/.dotfiles_old
 files=".bashrc .tmux.conf .Xresources"
 
 echo "Creating $dotbak for backup of any existing dotfiles in ~"
@@ -15,7 +15,7 @@ echo "...complete."
 
 for file in $files; do
     echo "Moving existing dotfiles from ~ to $dotbak"
-    mv ~/$file ~/dotfiles_old/
+    mv ~/$file ~/.dotfiles_old/
     echo "Creating symlink to $file in home dotdirectory."
     ln -s $dotdir/$file ~/$file
 done
